defa1c13f22f73aaa7bc9c6ab26224ae5f48e7d3
Uttam K Ukkoji <uttamukkoji@gmail.com>
2019-12-11 17:45:16 +0530

    Initial commit

---
+++ contentstack.model.generator/Model/Contenttype.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace contentstack.model.generator.Model
+{
+    public class Contenttype
+    {
+       public string Title { get; set; }
+
+       public string Uid { get; set; }
+
+       public List<Field> schema { get; set; }
+    }
+}
---
+++ contentstack.model.generator/Model/Field.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace contentstack.model.generator.Model
+{
+    public class Field
+    {
+        [JsonProperty(PropertyName = "display_name")]
+        public string DisplayName;
+
+        public string Uid;
+
+        [JsonProperty(PropertyName = "data_type")]
+        public string DataType;
+
+        [JsonProperty(PropertyName = "multiple")]
+        public bool IsMultiple;
+
+        [JsonProperty(PropertyName = "reference_to")]
+        public object ReferenceTo;
+
+        [JsonProperty(PropertyName = "field_metadata")]
+        public MetaData Fieldmetadata;
+
+    }
+}
